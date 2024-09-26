0f71662dd6a0aae3b83aefaa68945ad563e9136d
Mathias Krogkær Larsen <mala18@videndjurs.net>
2024-09-26 11:46:08 +0200

    second

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import Po from "./po";
 import "../css/style.scss";
+import data from "../assets/JSON/data.json"
 
 // **  IIFE: Immediately Invoked Function Expression  */
 
 (function () {
 
-  let init = new Po();
+  let init = new Po(data);
 
 })();
--- src/po.js
+++ src/po.js
@@ -1,8 +1,106 @@
+import * as THREE from "three";
 import World from "./world";
+import gsap from "gsap";
 
 class Po {
-    constructor() {
-        const world = new World();
+    constructor(data) {
+
+        const world = new World({
+            showCameraPos: false,
+            setCameraPos: [5, 2.6, 8.8],
+            showGrid: false,
+            ambientLight: false,
+            orbitControls: true,
+            showFloor: true
+        });
+
+        const spotLight = new THREE.SpotLight(0xffffff, 50);
+        spotLight.position.set(0, 5, 0);
+        spotLight.angle = .8;
+        spotLight.penumbra = 1;
+        spotLight.decay = 2;
+        spotLight.distance = 100;
+        spotLight.castShadow = true;
+
+        spotLight.shadow.mapSize.width = 2000;
+        spotLight.shadow.mapSize.height = 2000;
+        spotLight.shadow.camera.near = .5;
+        spotLight.shadow.camera.far = 500;
+        spotLight.shadow.focus = 1;
+        world.scene.add(spotLight)
+
+        const loader = new THREE.TextureLoader();
+        const materialCube = [
+            new THREE.MeshPhongMaterial({ map: loader.load('assets/IMG/one.png')}), //right
+            new THREE.MeshPhongMaterial({ map: loader.load('assets/IMG/two.png')}), //left
+            new THREE.MeshPhongMaterial({ map: loader.load('assets/IMG/three.png')}), //top
+            new THREE.MeshPhongMaterial({ map: loader.load('assets/IMG/four.png')}), //bottom
+            new THREE.MeshPhongMaterial({ map: loader.load('assets/IMG/five.png')}), //front
+            new THREE.MeshPhongMaterial({ map: loader.load('assets/IMG/six.png')}), //back
+        ]
+
+        const cubeInfo = [
+        {
+            texture: "assets/one.png",
+            position: [2, 0.5, -3],
+            rotation: -2
+        },
+
+        {
+            texture: "assets/two.png",
+            position: [2, 0.5, 2],
+            rotation: -2
+        },
+
+        {
+            texture: "assets/three.png",
+            position: [-2, 0.5, 2],
+            rotation: -2
+        },
+        
+        {
+            texture: "assets/four.png",
+            position: [-2, 0.5, -2.5],
+            rotation: -2
+        }
+    ]  
+
+        cubeInfo.forEach((item, index) => {
+            const geometryCube = new THREE.BoxGeometry(1, 1, 1);
+
+            this.cube = new THREE.Mesh(geometryCube, materialCube);
+            this.cube.castShadow = true;
+            this.cube.receiveShadow = true;
+            world.scene.add(this.cube);
+            this.cube.position.set(
+                item.position[0],
+                item.position[1],
+                item.position[2]
+            );
+            this.cube.rotation.y = item.rotation;
+
+            world.interactionManager.add(this.cube);
+
+            this.cube.addEventListener("click", (event) =>
+                this.moveObj(event, world)
+            );
+        });//END foreach
+    }//END constructor
+
+    moveObj(event, world) {
+        gsap.to(event.target.position, {
+            duration: 1,
+            x: 0.5,
+            y: 0.5,
+            repeat: 1,
+            yoyo: true
+        });
+        gsap.to(event.target.position, {
+            duration: 1,
+            y: 0.5,
+            repeat: 1,
+            yoyo: true
+        });
     }
 }
 
